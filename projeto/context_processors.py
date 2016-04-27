81e6e5d05abd202d2c8c7bf77c68ea5e6fda1c10
Marcelo Soares Souza <marcelo.soares@colaborador.embrapa.br>
2016-04-27 16:53:07 -0300

    Minor Adjustments and Fixes

---
+++ projeto/context_processors.py
@@ -0,0 +1,5 @@
+from platform import python_version
+from django import get_version
+
+def django_version(request):
+    return { 'django_version': get_version() + ' with Python ' + python_version() }
