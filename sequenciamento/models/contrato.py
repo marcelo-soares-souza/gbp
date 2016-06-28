228ed165cf334f7ed10e8292339da17dff509a22
Marcelo Soares Souza <marcelo.soares@colaborador.embrapa.br>
2016-06-28 16:42:47 -0300

    Fix minor error in Contrato, put default date

--- sequenciamento/models/contrato.py
+++ sequenciamento/models/contrato.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 from projeto.models.projeto import Projeto
 from projeto.models.template import TemplateModelMixin
@@ -30,7 +31,7 @@ class Contrato(models.Model, TemplateModelMixin):
     status_contrato = models.CharField(max_length=64, choices=STATUS_CONTRATO, null=True, blank=True)
     status_pagamento = models.CharField(max_length=64, choices=STATUS_PAGAMENTO, null=True, blank=True)
     empresa_executora = models.CharField(max_length=256, null=True, blank=True)
-    data_contratacao = models.DateField(null=True, blank=True)
+    data_contratacao = models.DateField(null=True, blank=True, default=timezone.now)
     status_cgen = models.CharField(max_length=64, choices=STATUS_CGEN, null=True, blank=True)
     ttm = models.CharField(max_length=256, null=True, blank=True)
     contato_gestor = models.CharField(max_length=256, null=True, blank=True)
