8b04e2d3e402871097eec14f3efda19e754f6ded
Marcelo Soares Souza <marcelo.soares@colaborador.embrapa.br>
2016-09-08 10:26:14 -0300

    Added basic permission system based on groups

--- projeto/templatetags/gbp_filters.py
+++ projeto/templatetags/gbp_filters.py
@@ -1,4 +1,6 @@
 from django import template
+from django.contrib.auth.models import Group
+
 
 register = template.Library()
 
@@ -9,3 +11,9 @@ def verifyfullname(value):
         return '%s %s' % (value.first_name, value.last_name)
     else:
         return value
+
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    group = Group.objects.get(name=group_name)
+    return True if group in user.groups.all() else False
