7154fad829cad681826cc56cf8c3e42433107b5e
Marcelo Soares Souza <marcelo@riseup.net>
2016-09-12 15:45:50 -0300

    Minor fix in passenger module file

--- public/passenger_wsgi.py
+++ public/passenger_wsgi.py
@@ -1,9 +1,10 @@
-import sys, os
+import sys
+import os
+import gbp.wsgi
 
 INTERP = "/var/www/python/envs/gbp/bin/python"
 
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-
-import gbp.wsgi
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
 
 application = gbp.wsgi.application
