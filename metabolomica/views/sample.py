eac1ce4a21a375ed4ac7e2be98e838e9746b365e
Gustavo-Martins <gustavo@lbb-srvlnx-vm-devel01>
2016-10-28 16:27:01 -0200

    Updated Metabolomica's Sample form
    Added comments with translations pt-BR to en-US,
    and translated interface.

--- metabolomica/views/sample.py
+++ metabolomica/views/sample.py
@@ -12,8 +12,8 @@ class SampleList(LoggedInMixin, SortableListView):
                 'name':
                 {'default_direction': '', 'verbose_name': 'Name'},
                 'data_atualizado':
-                {'default_direction': '', 'verbose_name': 'Atualizado Em'}
-                          }
+                {'default_direction': '', 'verbose_name': 'Modified'}
+                           }
 
     default_sort_field = 'name'
     paginate_by = 5
